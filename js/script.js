fbc64f9aadbce88cfbd1b6c1c93771aa9d7d0153
Raphael Pimenta <raphael.pimentagomes@hotmail.com>
2023-08-27 23:58:22 -0300

    lista adicionada

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 import initmenuHamburguer from "./module/menuHamburguer.js";
 import initAnimaNumeros from "./module/animaNumero.js";
-// import initanimacaoScroll from "./module/animaScroll.js";
 
 
 initmenuHamburguer();
 initAnimaNumeros();
-// initanimacaoScroll();
