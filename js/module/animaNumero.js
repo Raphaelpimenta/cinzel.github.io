4c06ab97689df2af375424301b688199e52a75c5
Raphael Pimenta <raphael.pimentagomes@hotmail.com>
2023-09-18 02:05:42 -0300

    ajuste em números

--- js/module/animaNumero.js
+++ js/module/animaNumero.js
@@ -16,7 +16,7 @@ export default function initAnimaNumeros(){
                     item.innerHTML = total;
                     clearInterval(timer)
                 }
-            }, 65 * Math.random())
+            }, 75 * Math.random())
         })
 
     }
